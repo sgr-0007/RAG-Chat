bbbd28f48bf83420546dd91c0c6a71bf690046c2
Sagar Honnenahalli somashekhar <shonnenahallisomashekhar1@sheffield.ac.uk>
2024-09-30 04:38:54 +0100

    Pdf read

---
+++ src/app/api/ex5/route.ts
@@ -0,0 +1,89 @@
+import {
+    Message as VercelChatMessage,
+    StreamingTextResponse,
+    createStreamDataTransformer
+} from 'ai';
+import { ChatOpenAI } from '@langchain/openai';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { HttpResponseOutputParser } from 'langchain/output_parsers';
+
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'; // Import the PDFLoader
+import { RunnableSequence } from '@langchain/core/runnables';
+import { formatDocumentsAsString } from 'langchain/util/document';
+import { CharacterTextSplitter } from 'langchain/text_splitter';
+
+export const dynamic = 'force-dynamic';
+
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+    return `${message.role}: ${message.content}`;
+};
+
+const TEMPLATE = `Answer the user's questions based only on the following context. If the answer is not in the context, reply politely that you do not have that information available.:
+==============================
+Context: {context}
+==============================
+Current conversation: {chat_history}
+
+user: {question}
+assistant:`;
+
+
+export async function POST(req: Request) {
+    try {
+        // Extract the `messages` from the body of the request
+        const { messages } = await req.json();
+
+        const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+
+        const currentMessageContent = messages[messages.length - 1].content;
+
+        // Load the PDF file using PDFLoader
+        const loader = new PDFLoader("src/data/Rdf-Report.pdf");  // Replace with the path to your PDF
+
+        const docs = await loader.load(); // Load the content of the PDF
+
+        const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+
+        const model = new ChatOpenAI({
+            apiKey: process.env.OPENAI_API_KEY!,
+            model: 'gpt-4',
+            temperature: 0,
+            streaming: true,
+            verbose: true,
+        });
+
+        /**
+       * Chat models stream message chunks rather than bytes, so this
+       * output parser handles serialization and encoding.
+       */
+        const parser = new HttpResponseOutputParser();
+
+        const chain = RunnableSequence.from([
+            {
+                question: (input) => input.question,
+                chat_history: (input) => input.chat_history,
+                context: () => formatDocumentsAsString(docs),
+            },
+            prompt,
+            model, 
+            parser,
+        ]);
+
+        // Convert the response into a friendly text-stream
+        const stream = await chain.stream({
+            chat_history: formattedPreviousMessages.join('\n'),
+            question: currentMessageContent,
+        });
+
+        // Respond with the stream
+        return new StreamingTextResponse(
+            stream.pipeThrough(createStreamDataTransformer()),
+        );
+    } catch (e: any) {
+        return Response.json({ error: e.message }, { status: e.status ?? 500 });
+    }
+}
